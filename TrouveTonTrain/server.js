6b99839ba5b7343a488fa985dc223c2ce185dccf
thomasj81 <thomas.jamme@master-stic.fr>
2020-04-16 21:00:27 +0200

    my first commit'

--- TrouveTonTrain/server.js
+++ TrouveTonTrain/server.js
@@ -1,16 +1,14 @@
-// Installer un serveur express
-const express = require ('express');
-const path = require ('path');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const app = express ();
+// Serve static files....
+app.use(express.static(__dirname + '/dist/TrouveTonTrain'));
 
-// Ne sert que les fichiers statiques du répertoire dist
-app.use (express.static (__ dirname + '/ dist / <name-of-app>'));
-
-app.get ('/ *', fonction (req, res) {
-    
-res.sendFile (path.join (__ dirname + '/ dist / <name-of-app> /index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/TrouveTonTrain/index.html'));
 });
 
-// Démarrez l'application en écoutant sur le port Heroku par défaut
-app.listen (process.env.PORT || 8080);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
